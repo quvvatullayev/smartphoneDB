aaefb97222f164ca55498a658e5a04643f9ac832
ZARIFMAC <uzbzarif@gmail.com>
2022-10-29 18:26:53 +0500

    model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,30 @@
+
+from email.mime import image
+from platform import release
+from venv import create
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    logo = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    website = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    ram = models.IntegerField()
+    memory = models.IntegerField()
+    price = models.FloatField()
+    image = models.CharField(max_length=100)
+    release_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
